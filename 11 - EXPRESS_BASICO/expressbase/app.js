81b7d809d92409ee3d0158979e5f72885fa5472e
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 18:18:15 -0300

    enrrutado segun verbos

--- 11 - EXPRESS_BASICO/expressbase/app.js
+++ 11 - EXPRESS_BASICO/expressbase/app.js
@@ -4,9 +4,27 @@ var express = require('express');
 //inicializar express
 var app = express();
 
+//sin importar el verbo http vamos a generar una respuesta
 app.all("/",function(peticion, respuesta){
   respuesta.send("hola");
 })
+
+//esta aplicacion puede mostrar comportamiento de una API RESTfull
+//comportamiento get
+app.get('/acerca',function(peticion,respuesta){
+  respuesta.send("ACERCA");
+})
+
+//comportamiento post
+app.post('/acerca',function(peticion,respuesta){
+  respuesta.send("ACERCA");
+})
+
+//comportamiento put
+app.put('/acerca',function(peticion,respuesta){
+  respuesta.send("ACERCA");
+})
+
 
 //asignamos el puerto especifico y ejecutamos una funcion cada vez que se ejecute
 // Cada vez que escuchemos alguna petición, vamos a enrutar el tráfico hacia diferentes secciones, 
