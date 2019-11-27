0c4f8cbf0d90ff7d0d196bd74519e056e41936fb
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 13:28:01 -0300

    Funciones y métodos en Node.js

--- 02 - Iniciar proyectos/app.js
+++ 02 - Iniciar proyectos/app.js
@@ -1,1 +1,20 @@
-console.log("tarea ejecutada");
+function datosSimples (){
+    var nombre="Teodoro";
+    nombre=10;
+    console.log(nombre);
+}
+
+function datosCompuestos(){
+    let lista = ["uno","dos","tres"];
+
+    let curso = {
+        tema: "NodeJs",
+        leccion: "datos compuestos"
+    };
+
+    console.log(curso.tema)
+
+}
+
+datosSimples();
+datosCompuestos();
---
+++ 02 - Iniciar proyectos/funcion.js
@@ -0,0 +1,17 @@
+function saludo(){
+    return "hola";
+}
+
+
+let calculo = {
+    duplicar: function(valor1){
+        return valor1*2;
+    },
+    dividir: function(valor1){
+        return valor1/2;
+    },
+
+}
+
+console.log(saludo());
+console.log(calculo.duplicar(10),calculo.dividir(10));
