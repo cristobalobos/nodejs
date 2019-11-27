6a46349ffd2a2b85429060b2bd4a91cc62cabece
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 16:55:45 -0300

    paquetes preinstalados node

---
+++ 08- Requiere Modulo Local/app.js
@@ -0,0 +1,9 @@
+var util = require('util')
+//modulo predeterminado que no necesita ser importado
+var prueba = "10"
+var test = 19
+//comprueba si es string
+console.log(util.isString(prueba));
+console.log(util.isString(test));
+
+
