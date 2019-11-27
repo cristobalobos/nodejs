785f4dca50bea42ec5749164e725b743e8c06ffc
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 13:38:23 -0300

    Buffer y Process a examen

--- 02 - Iniciar proyectos/app.js
+++ 02 - Iniciar proyectos/app.js
@@ -13,7 +13,6 @@ function datosCompuestos(){
     };
 
     console.log(curso.tema)
-
 }
 
 datosSimples();
---
+++ 02 - Iniciar proyectos/bufferprocess.js
@@ -0,0 +1,15 @@
+// Es una salida que nos muestra los elementos binarios
+console.log(Buffer(10));
+
+// Te entrega información del proceso donde se esta ejecutando
+console.log(process)
+
+// SO
+console.log(process.platform)
+
+//RUTA
+console.log(process.execPath);
+
+//Nuevas propiedades
+process.env.colorFavorito = "rojo"
+console.log(process.env.colorFavorito);
