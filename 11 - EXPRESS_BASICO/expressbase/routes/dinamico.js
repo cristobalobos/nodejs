d2f779e52f8cd2a1e1f3b4c82ef03cc627535676
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 22:33:05 -0300

    desplegar variables pug

--- 11 - EXPRESS_BASICO/expressbase/routes/dinamico.js
+++ 11 - EXPRESS_BASICO/expressbase/routes/dinamico.js
@@ -6,8 +6,14 @@ router.get("/", function(peticion, respuesta){
     respuesta.send("dinamico");
 })
 
-router.get("/:datoURL", function(peticion, respuesta){
-    respuesta.send("informacion dinamica: " + peticion.params.datoURL);
+router.get("/:datoURL/:ID", function(peticion, respuesta){
+   // respuesta.send("informacion dinamica: " + peticion.params.datoURL);
+   respuesta.render('dinamico',{
+       datos: {
+           titulo: peticion.params.datoURL,
+           ID: peticion.params.ID
+       }
+   })
 })
 
 module.exports = router;
