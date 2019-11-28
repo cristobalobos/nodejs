5a2a6f71191a13753d94f465621716d18bb97363
cristobalobos <cristobal.lobost@gmail.com>
2019-11-27 21:58:46 -0300

    rutas dinamicas Express

--- 11 - EXPRESS_BASICO/expressbase/app.js
+++ 11 - EXPRESS_BASICO/expressbase/app.js
@@ -1,32 +1,45 @@
-//invocar express
+var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//inicializar express
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var pruebaRouter = require('./routes/prueba');
+var dinamicoRouter = require('./routes/dinamico');
+
 var app = express();
 
-//sin importar el verbo http vamos a generar una respuesta
-app.all("/",function(peticion, respuesta){
-  respuesta.send("hola");
-})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-//esta aplicacion puede mostrar comportamiento de una API RESTfull
-//comportamiento get
-app.get('/acerca',function(peticion,respuesta){
-  respuesta.send("ACERCA");
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//comportamiento post
-app.post('/acerca',function(peticion,respuesta){
-  respuesta.send("ACERCA");
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/prueba' , pruebaRouter );
+app.use('/dinamico',dinamicoRouter);
 
-//comportamiento put
-app.put('/acerca',function(peticion,respuesta){
-  respuesta.send("ACERCA");
-})
-
-
-//asignamos el puerto especifico y ejecutamos una funcion cada vez que se ejecute
-// Cada vez que escuchemos alguna petición, vamos a enrutar el tráfico hacia diferentes secciones, 
-var server = app.listen(3000, function(){  
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
